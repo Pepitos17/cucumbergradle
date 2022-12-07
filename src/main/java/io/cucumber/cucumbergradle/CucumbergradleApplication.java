1f4bad34de19a95db143db0f2d087ff6df57c7d7
Pepitos17 <msogodogo@gmail.com>
2022-12-07 21:40:29 +0100

    Initial commit

---
+++ src/main/java/io/cucumber/cucumbergradle/CucumbergradleApplication.java
@@ -0,0 +1,13 @@
+package io.cucumber.cucumbergradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CucumbergradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CucumbergradleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/cucumber/cucumbergradle/CucumbergradleApplicationTests.java
@@ -0,0 +1,13 @@
+package io.cucumber.cucumbergradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CucumbergradleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
